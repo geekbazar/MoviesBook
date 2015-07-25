0ce43a8affdd4c44f1a895a74c5280ac42a05094
geekbazar <geekbazar47@gmail.com>
2015-07-24 18:13:50 -0700

    Implemented client app owin self host

---
+++ Movies.Api.Owin.SelfHost/Movies.UI.Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.IO;
+using System.Reflection;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.FileSystems;
+
+[assembly: OwinStartup(typeof(Movies.Api.Owin.SelfHost.ClientConfiguration))]
+
+namespace Movies.Api.Owin.SelfHost
+{
+    public class ClientConfiguration
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string clientPath = Path.Combine(Directory.GetParent(Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName).FullName, "Movies.UI");
+
+            var fileServerOptions = new FileServerOptions
+                                            {
+                                                EnableDefaultFiles = true,
+                                                FileSystem = new PhysicalFileSystem(clientPath)
+                                            };
+            fileServerOptions.StaticFileOptions.FileSystem = new PhysicalFileSystem(clientPath);
+            fileServerOptions.StaticFileOptions.ServeUnknownFileTypes = true;
+            fileServerOptions.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+
+
+            app.UseFileServer(fileServerOptions);
+        }
+    }
+}
