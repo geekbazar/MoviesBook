2a0e44a99b3f7dd64ef369e426ddaa65e8a1f4ae
geekbazar47 <geekbazar47@gmail.com>
2015-07-26 00:25:38 +0530

    Client configuration

--- Movies.Api.Owin.SelfHost/Program.cs
+++ Movies.Api.Owin.SelfHost/Program.cs
@@ -18,12 +18,19 @@ namespace Movies.Api.Owin.SelfHost
 
             Console.WriteLine("Starting web server");
             WebApp.Start<Startup>(serverOptions);
-            Console.WriteLine("Server running at {0} - press enter to quit.", serverOptions.Port);
-            Console.ReadLine();
+            Console.WriteLine("Server running at {0}", serverOptions.Port);
             #endregion
 
             #region Client Configuration
-            
+
+            StartOptions clientOptions = new StartOptions();
+            clientOptions.Port = 7474;
+            clientOptions.Urls.Add("localhost");
+
+            Console.WriteLine("Starting web client");
+            WebApp.Start<ClientConfiguration>(clientOptions);
+            Console.WriteLine("Client running at {0} - press enter to quit.", clientOptions.Port);
+            Console.ReadLine();
             #endregion
         }
     }
